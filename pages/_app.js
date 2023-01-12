4bf6edeca550e9c87d35af5e6b953a9bac6ae8c5
Igor <w0rz@protonmail.com>
2023-01-11 21:18:12 -0300

    Head moved from index.js to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { Provider } from "react-redux";
+import store from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import Head from 'next/head'
+let persistor = persistStore(store);
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return(
+    <>
+      <Head>
+        <title>Shop Pay</title>
+        <meta name="description" content="Onlne shopping service." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+<Provider store={store}>
+    <PersistGate Loading={null} persistor={persistor}>
+   <Component {...pageProps} />
+</PersistGate>
+    </Provider>
+    </>
+  )
 }
+
+export default App;
