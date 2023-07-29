d4de7ae03bad27cd8d6a75b31bd884d9c0dbde02
Igor <w0rz@protonmail.com>
2023-07-29 00:34:03 -0300

    Navbar

---
+++ components/Header/Top.jsx
@@ -0,0 +1,62 @@
+import styles from "./styles.module.scss";
+import {MdSecurity} from "react-icons/md";
+import {BsSuitHeart} from "react-icons/bs";
+import {RiAccountPinCircleLine, RiArrowDropDownFill} from "react-icons/ri";
+import Link from 'next/link';
+import { useState } from "react";
+import UserMenu from "./UserMenu";
+
+export default function Top() {
+const [loggedIn, setLoggedIn] = useState(false);
+  return (
+  <div className={styles.top}>  
+    <div className={styles.top__container}>
+    <div></div>
+      <ul className={styles.top__list}>
+        <li>
+<img src="https://opensource.com/sites/default/files/images/life-uploads/love.png"
+alt=""
+/>
+          <span>Penguin / usd</span>
+        </li>
+        <li>
+          <MdSecurity/>
+          <span>Buyer Protection</span>
+        </li>
+          <li>
+           <span>Customer Service</span> 
+          </li>
+          <li>
+              <span>Help</span>
+          </li>
+          <li>
+<BsSuitHeart />
+            <Link href="/profile/wishlist">
+            <span>Whishlist</span>
+          </Link>
+          </li>
+          <li>
+          {loggedIn ? (
+          <li>
+            <div className={styles.flex}>
+              <img src="https://cdn2.iconfinder.com/data/icons/circle-avatars-1/128/050_girl_avatar_profile_woman_suit_student_officer-512.png" alt="" />
+                <span>Milena</span>
+              <RiArrowDropDownFill />
+            </div>
+          </li>
+          ) : (
+          <li>
+            <div className={styles.flex}>
+              <RiAccountPinCircleLine />
+              <span>Account</span>
+              <RiArrowDropDownFill />
+            </div>
+          </li>
+          )}
+            <UserMenu loggedIn={loggedIn} />
+          </li>
+      </ul>
+    </div>
+  </div>
+  )
+}
