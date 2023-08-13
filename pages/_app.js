740177059cd2176c6bbd94c74b1c32f1ed4779d8
Igor <w0rz@protonmail.com>
2023-08-12 23:14:05 -0300

    next-auth & mongodb adapter

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
 import { Provider } from "react-redux";
 import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
-import Head from 'next/head'
+import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+
 let persistor = persistStore(store);
 
-function App({ Component, pageProps }) {
-  return(
+function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
     <>
       <Head>
         <title>Shop Pay</title>
@@ -15,13 +17,15 @@ function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<Provider store={store}>
-    <PersistGate Loading={null} persistor={persistor}>
-   <Component {...pageProps} />
-</PersistGate>
-    </Provider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <PersistGate Loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </SessionProvider>
     </>
-  )
+  );
 }
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Header from "../components/Header/";
 import Footer from "../components/Footer/";
 import axios from "axios";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home({ country }) {
+  const { data: session } = useSession();
   return (
     <main>
       <Header country={country} />
+      {session ? "you are logged in" : "you are not logged in"}
       <Footer country={country} />
     </main>
   );
 }
 export async function getServerSideProps() {
   let data = await axios
-    .get(
-      `https://api.ipregistry.co/?key=${process.env.NEXT_PUBLIC_REGISTRY_API_KEY}`,
-    )
+    .get(`https://api.ipregistry.co/?key=${process.env.REGISTRY_API_KEY}`)
     .then((res) => {
       return res.data.location.country;
     })
