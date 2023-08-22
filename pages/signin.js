6ec4c6bde01b831ee70cc53076148e6937e0d968
Igor <w0rz@protonmail.com>
2023-08-21 22:45:34 -0300

    custom input with yup & button submit

--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,86 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styles from "../styles/Signin.module.scss";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import Link from "next/link";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import LoginInput from "../components/Inputs/LoginInput";
+import { useState } from "react";
+import RoundButton from "../components/Buttons/RoundButton";
+
+const initialValues = {
+  login_email: "",
+  login_password: "",
+};
+
 export default function signin() {
-  return <div>signin</div>;
+  const [user, setUser] = useState(initialValues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  console.log(user);
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email address is required.")
+      .email("Please enter a valid email address"),
+    login_password: Yup.string().required("Please enter a password"),
+  });
+  return (
+    <>
+      <Header country="Brazil" />
+      <div className={styles.login}>
+        <div className={styles.login__container}>
+          <div className={styles.login__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt />
+            </div>
+            <span>
+              You'd be happy to join us! <Link href="/">Go to store</Link>
+            </span>
+          </div>
+          <div className={styles.login__form}>
+            <h1>Sign in</h1>
+            <p>
+              Get access to one of the best E-shopping services in the world.
+            </p>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="email"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <RoundButton type="submit" text="Sign in" />
+                  <div className={styles.forgot}>
+                    <Link href="/forget">Forgot password ?</Link>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+      <Footer country="Brazil" />
+    </>
+  );
 }
