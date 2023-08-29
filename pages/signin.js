fc95aacdcc7c9eec3269f5c11ec2439a60c36c0d
Igor <w0rz@protonmail.com>
2023-08-29 19:20:51 -0300

    signin & signup validation

--- pages/signin.js
+++ pages/signin.js
@@ -15,6 +15,10 @@ import { BsGoogle } from "react-icons/bs";
 const initialValues = {
   login_email: "",
   login_password: "",
+  full_name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
 };
 const rIcon = {
   auth0: SiAuth0,
@@ -25,7 +29,14 @@ const rIcon = {
 export default function signin({ providers }) {
   console.log(providers);
   const [user, setUser] = useState(initialValues);
-  const { login_email, login_password } = user;
+  const {
+    login_email,
+    login_password,
+    full_name,
+    email,
+    password,
+    confirm_password,
+  } = user;
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
@@ -35,6 +46,27 @@ export default function signin({ providers }) {
       .required("Email address is required.")
       .email("Please enter a valid email address"),
     login_password: Yup.string().required("Please enter a password"),
+  });
+  const registerValidation = Yup.object({
+    full_name: Yup.string()
+      .required("what's your name?")
+      .min(2, "First name must be between 2 and 16 characters.")
+      .max(16, "First name must be between 2 and 16 characters.")
+      .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
+    email: Yup.string()
+      .required(
+        "You'll need this when you login and if you ever need to reset your password.",
+      )
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six number, letters and punctuation marks(such as ! and &).",
+      )
+      .min(6, "Password must be at least 6 characters longer")
+      .max(46, "Password can't be longer than 46 characters"),
+    confirm_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
   });
   return (
     <>
@@ -106,6 +138,62 @@ export default function signin({ providers }) {
             </div>
           </div>
         </div>
+
+        {/* Signup */}
+
+        <div className={styles.login__container}>
+          <div className={styles.login__form}>
+            <h1>Sign up</h1>
+            <p>
+              Get access to one of the best E-shopping services in the world.
+            </p>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                full_name,
+                email,
+                password,
+                confirm_password,
+              }}
+              validationSchema={registerValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="full_name"
+                    icon="user"
+                    placeholder="Full Name"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="confirm_password"
+                    icon="password"
+                    placeholder="Re-Type Password"
+                    onChange={handleChange}
+                  />
+                  <RoundButton type="submit" text="Sign up" />
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+        {/* Sing up End */}
       </div>
       <Footer country="Brazil" />
     </>
