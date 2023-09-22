b13244f9128be6f140c3a33e51f3f7cb27d1fdec
Igor <w0rz@protonmail.com>
2023-09-21 23:44:07 -0300

    forgot handler

---
+++ pages/api/auth/forgot.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import db from "../../../utils/db";
+import validateEmail from "../../../utils/validation";
+import User from "../../../models/User";
+import { createActivationToken, createResetToken } from "../../../utils/tokens";
+import { sendEmail } from "../../../utils/sendEmails";
+import { resetEmailTemplate } from "../../../emails/resetEmailTemplate";
+
+export default async function handler(req, res) {
+  try {
+    await db.connectDb();
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "This email does not exist." });
+    } else {
+    }
+    const user_id = createResetToken({
+      id: user._id.toString(),
+    });
+    const url = `${process.env.BASE_URL}/auth/reset/${user_id}`;
+    sendEmail(email, url, "", "Activate your account.", resetEmailTemplate);
+    await db.disconnectDb();
+    res.json({
+      message:
+        "An email has been sent to you. Please use it to reset your password.",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
