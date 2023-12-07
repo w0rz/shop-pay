b60b06050eef470c17047da226301af4f76792b3
Ig0r <w0rz@protonmail.com>
2023-12-07 20:57:51 -0300

    menu component

--- components/Home/Main/index.jsx
+++ components/Home/Main/index.jsx
@@ -1,12 +1,13 @@
 import MainSwiper from "./swiper";
 import Offers from "./offers";
+import Menu from "./menu";
 import styles from "./styles.module.scss";
 
 export default function Main() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>Header</div>
-      <div className={styles.menu}>Menu</div>
+      <Menu />
       <MainSwiper />
       <Offers />
       <div className={styles.user}>User</div>
